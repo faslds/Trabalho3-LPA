18e7746ecb807e95fa12d9d62d3d9d055b0ae21c
faslds <felipe.senger.souza@usp.br>
2015-12-01 19:28:13 -0200

    v0.2

--- ex-matriz_esparsa.c
+++ ex-matriz_esparsa.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct linha
+{
+	int numero;
+	int coluna
+	struct linha *prox;
+}
+
 struct coluna
 {
 	int linha;
@@ -8,12 +15,47 @@ struct coluna
 	struct coluna *prox;
 }
 
-struct linha
+void remove (struct coluna *raiz, int m, int n)
 {
-	int numero;
-	int coluna
-	struct linha *prox;
+	struct linha *a = NULL;
+	struct linha *b = NULL;
+	struct linha *c = NULL;
+	struct coluna *pont = raiz;
+	while(pont->linha != m)
+	{
+		pont = pont->prox;
+	}
+	a = pont->line;
+	//b = pont->line;
+	
+	//if testar se existe alguma coisa na linha
+	
+	while(a->coluna > n)
+	{
+		if(a->prox != NULL)
+		{
+			b = a;
+			a = a->prox;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	if(a->coluna == n)
+	{
+		b->prox = a->prox;
+		printf("O número %d foi removido com sucesso!", a->numero);
+		free(a);
+	}
+	else
+	{
+		return -1;
+	}
+	
 }
+
+
 
 void insert(int k, int m, int n, struct coluna *raiz)
 {
@@ -37,22 +79,33 @@ void insert(int k, int m, int n, struct coluna *raiz)
 	else 
 	{
 		a = pont->line;
-		while(a->prox != NULL && a->coluna < n)
+		if (a->coluna > n)
 		{
-			a = a->prox;
-			//i++;
+			b = (struct linha*) malloc(sizeof(struct linha));
+			b->numero = k;
+			b->coluna = n;
+			b->prox = a;
+			pont->line = b;
 		}
-		b = (struct linha*) malloc(sizeof(struct linha));
-		b->numero = k;
-		b->coluna = n;
-		b->prox = a->prox;
-		a->prox = b;
+		else
+		{
+			while(a->prox != NULL && a->coluna < n)
+			{
+				a = a->prox;
+				//i++;
+			}
+			b = (struct linha*) malloc(sizeof(struct linha));
+			b->numero = k;
+			b->coluna = n;
+			b->prox = a->prox;
+			a->prox = b;
+		}
 	}
 }
 
 int main ()
 {
-	int m, n, i;
+	int m, n, i, escolha;
 	printf("Qual o tamanho da sua matriz? Digite na seguinte ordem: ´numero de linhas´ ´ENTER´ ´numero de colunas´ ´ENTER´");
 	scanf("%d", &m);
 	while (m < 1)
@@ -71,12 +124,18 @@ int main ()
 	struct coluna *pont = raiz;
 	raiz->prox = NULL;
 	raiz->line = NULL;
-	for (i = 0; i < m; m++)
+	raiz->linha = 0;
+	for (i = 1; i < m; i++)
 	{
 		pont->prox = (struct coluna *)malloc(sizeof(struct coluna));
 		pont = pont->prox;
 		pont->line = NULL;
 		pont->prox = NULL;
+		pont->linha = i;
 	}
-	
+	while (escolha != )
+	{
+		printf("\n\nO que voce deseja fazer? Digite o numero correspondente!\n");
+		printf("")
+	}
 }
